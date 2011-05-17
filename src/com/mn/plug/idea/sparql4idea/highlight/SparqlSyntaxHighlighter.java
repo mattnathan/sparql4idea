b041d0f9f24216f1c673c6a5577226f310642b21
Matt Nathan <matt.nathan@pressassociation.com>
2011-05-17 14:38:11 +0100

    Add syntax highlighter class

---
+++ src/com/mn/plug/idea/sparql4idea/highlight/SparqlSyntaxHighlighter.java
@@ -0,0 +1,46 @@
+package com.mn.plug.idea.sparql4idea.highlight;
+
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.HighlighterColors;
+import com.intellij.openapi.editor.SyntaxHighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.tree.IElementType;
+import com.mn.plug.idea.sparql4idea.lang.lexer.SparqlLexer;
+import com.mn.plug.idea.sparql4idea.lang.lexer.SparqlTokenTypeSets;
+import com.mn.plug.idea.sparql4idea.lang.lexer.SparqlTokenTypes;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.mn.plug.idea.sparql4idea.lang.lexer.SparqlTokenTypes.*;
+
+/**
+ * Syntax highlighter.
+ *
+ * @author Matt Nathan
+ */
+public class SparqlSyntaxHighlighter extends SyntaxHighlighterBase {
+
+  private static final Map<IElementType, TextAttributesKey> ATTRIBUTES = new HashMap<IElementType, TextAttributesKey>();
+
+  static {
+    // setup the styles
+    ATTRIBUTES.put(BAD_CHARACTER, HighlighterColors.BAD_CHARACTER);
+    ATTRIBUTES.put(WHITE_SPACE, HighlighterColors.TEXT);
+    fillMap(ATTRIBUTES, SparqlTokenTypeSets.KEYWORDS, SyntaxHighlighterColors.KEYWORD);
+  }
+
+  @NotNull
+  @Override
+  public Lexer getHighlightingLexer() {
+    return new SparqlLexer();
+  }
+
+  @NotNull
+  @Override
+  public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
+    return pack(ATTRIBUTES.get(tokenType));
+  }
+}
---
+++ src/com/mn/plug/idea/sparql4idea/lang/lexer/SparqlTokenTypeSets.java
@@ -0,0 +1,48 @@
+package com.mn.plug.idea.sparql4idea.lang.lexer;
+
+import com.intellij.psi.tree.TokenSet;
+
+import static com.mn.plug.idea.sparql4idea.lang.lexer.SparqlTokenTypes.*;
+
+/**
+ * Sets of token types for easy use.
+ *
+ * @author Matt Nathan
+ */
+public interface SparqlTokenTypeSets {
+
+  TokenSet COMMENTS = TokenSet.create();
+  TokenSet WHITESPACE = TokenSet.create(WHITE_SPACE);
+  TokenSet STRING_LITERALS = TokenSet.create();
+  TokenSet KEYWORDS = TokenSet.create(
+          KW_BASE,
+          KW_PREFIX,
+          KW_SELECT,
+          KW_CONSTRUCT,
+          KW_DESCRIBE,
+          KW_ORDER,
+          KW_BY,
+          KW_LIMIT,
+          KW_OFFSET,
+          KW_DISTINCT,
+          KW_REDUCED,
+          KW_FROM,
+          KW_NAMED,
+          KW_WHERE,
+          KW_GRAPH,
+          KW_OPTIONAL,
+          KW_UNION,
+          KW_FILTER,
+          KW_A,
+          KW_STR,
+          KW_LANG,
+          KW_LANGMATCHES,
+          KW_DATATYPE,
+          KW_BOUND,
+          KW_SAME_TERM,
+          KW_IS_URI,
+          KW_IS_IRI,
+          KW_IS_LITERAL,
+          KW_REGEX
+  );
+}
