f5efd2b73d8f931e7f0e8743558f87d7858c1305
Matt Nathan <matt.nathan@pressassociation.com>
2011-05-17 11:44:11 +0100

    First lexer support

---
+++ src/com/mn/plug/idea/sparql4idea/lang/lexer/SparqlLexer.java
@@ -0,0 +1,17 @@
+package com.mn.plug.idea.sparql4idea.lang.lexer;
+
+import com.intellij.lexer.FlexAdapter;
+import com.intellij.lexer.FlexLexer;
+
+/**
+ * General lexer for sparql language
+ *
+ * @author Matt Nathan
+ */
+public class SparqlLexer extends FlexAdapter {
+
+  public SparqlLexer() {
+    // todo: add the proper lexer
+    super(null);
+  }
+}
