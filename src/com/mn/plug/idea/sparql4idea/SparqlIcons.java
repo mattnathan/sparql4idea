ab13fca662555d3552b4d54b9ac2e8d986114931
Matt Nathan <matt.nathan@pressassociation.com>
2011-05-17 11:25:12 +0100

    Add the file icon (initial prototype)

---
+++ src/com/mn/plug/idea/sparql4idea/SparqlIcons.java
@@ -0,0 +1,16 @@
+package com.mn.plug.idea.sparql4idea;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Set of icons for the language
+ *
+ * @author Matt Nathan
+ */
+public interface SparqlIcons {
+
+    Icon XQUERY_ICON_16x16 = IconLoader.getIcon("/icons/fileicon-16x16.png");
+
+}
