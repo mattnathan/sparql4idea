4d895aaae1bd3273d405a3ea2dcfb051c0c0a868
Matt Nathan <matt.nathan@pressassociation.com>
2011-05-17 11:29:28 +0100

    Add the filetype factory to the plugin conf

---
+++ src/com/mn/plug/idea/sparql4idea/SparqlFileTypeFactory.java
@@ -0,0 +1,18 @@
+package com.mn.plug.idea.sparql4idea;
+
+import com.intellij.openapi.fileTypes.FileTypeConsumer;
+import com.intellij.openapi.fileTypes.FileTypeFactory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Factory for sparql file types
+ *
+ * @author Matt Nathan
+ */
+public class SparqlFileTypeFactory extends FileTypeFactory {
+
+  @Override
+  public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
+
+  }
+}
