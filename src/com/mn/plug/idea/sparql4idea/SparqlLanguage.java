16a0d31438940edef75cd1e98d6d1a2bd1c59426
Matt Nathan <matt.nathan@pressassociation.com>
2011-05-17 11:13:15 +0100

    Add Language file

---
+++ src/com/mn/plug/idea/sparql4idea/SparqlLanguage.java
@@ -0,0 +1,15 @@
+package com.mn.plug.idea.sparql4idea;
+
+import com.intellij.lang.Language;
+
+/**
+ * The SPARQL Language
+ *
+ * @author Matt Nathan
+ */
+public class SparqlLanguage extends Language {
+
+    protected SparqlLanguage() {
+        super("SPARQL");
+    }
+}
