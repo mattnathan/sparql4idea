9cec2165e5cc92d6705ffa441309ab88d1a7f70d
Matt Nathan <matt.nathan@pressassociation.com>
2011-05-18 10:10:18 +0100

    Add basic file template support... hopefully this will change to a specialised action with the multi-select dialog in the future

--- src/com/mn/plug/idea/sparql4idea/actions/SparqlFileTemplatesFactory.java
+++ src/com/mn/plug/idea/sparql4idea/actions/SparqlFileTemplatesFactory.java
@@ -1,10 +1,35 @@
 package com.mn.plug.idea.sparql4idea.actions;
 
+import com.intellij.ide.fileTemplates.FileTemplateGroupDescriptor;
+import com.intellij.ide.fileTemplates.FileTemplateGroupDescriptorFactory;
+import com.mn.plug.idea.sparql4idea.SparqlIcons;
+
 /**
- * Generated JavaDoc Comment.
+ * Set of templates available to the sparql plugin
  *
  * @author Matt Nathan
  */
-public class SparqlFileTemplatesFactory  {
+public class SparqlFileTemplatesFactory implements FileTemplateGroupDescriptorFactory {
 
+  public static enum Templates {
+    SELECT("SPARQL Select");
+    private final String file;
+
+    Templates(String file) {
+      this.file = file;
+    }
+
+    public String getFile() {
+      return file;
+    }
+  }
+
+  @Override
+  public FileTemplateGroupDescriptor getFileTemplatesDescriptor() {
+    final FileTemplateGroupDescriptor result = new FileTemplateGroupDescriptor("SPARQL Files", SparqlIcons.XQUERY_ICON_16x16);
+    for (Templates template : Templates.values()) {
+      result.addTemplate(template.getFile());
+    }
+    return result;
+  }
 }
