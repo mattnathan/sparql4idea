4b1ffc5239cce0bff0bd30a1368447efd0aecd2b
Matt Nathan <matt.nathan@pressassociation.com>
2011-05-19 09:45:05 +0100

    Moved Verb to the lit package

--- src/com/mn/plug/idea/sparql4idea/lang/parser/parsing/lit/Verb.java
+++ src/com/mn/plug/idea/sparql4idea/lang/parser/parsing/lit/Verb.java
@@ -1,4 +1,4 @@
-package com.mn.plug.idea.sparql4idea.lang.parser.parsing.common;
+package com.mn.plug.idea.sparql4idea.lang.parser.parsing.lit;
 
 import com.intellij.lang.PsiBuilder;
 import com.mn.plug.idea.sparql4idea.lang.lexer.SparqlTokenTypes;
