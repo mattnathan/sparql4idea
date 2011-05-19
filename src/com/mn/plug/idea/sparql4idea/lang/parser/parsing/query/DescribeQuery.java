1c8ba80cae7a239ab2de74eb4a149a22d71cd2ce
Matt Nathan <matt.nathan@pressassociation.com>
2011-05-19 11:16:27 +0100

    Fill out the describe query

--- src/com/mn/plug/idea/sparql4idea/lang/parser/parsing/query/DescribeQuery.java
+++ src/com/mn/plug/idea/sparql4idea/lang/parser/parsing/query/DescribeQuery.java
@@ -1,6 +1,12 @@
 package com.mn.plug.idea.sparql4idea.lang.parser.parsing.query;
 
 import com.intellij.lang.PsiBuilder;
+import com.mn.plug.idea.sparql4idea.lang.lexer.SparqlTokenTypes;
+import com.mn.plug.idea.sparql4idea.lang.parser.SparqlElementTypes;
+import com.mn.plug.idea.sparql4idea.lang.parser.parsing.common.SolutionModifiers;
+import com.mn.plug.idea.sparql4idea.lang.parser.parsing.common.WhereClause;
+import com.mn.plug.idea.sparql4idea.lang.parser.parsing.lit.Literals;
+import com.mn.plug.idea.sparql4idea.lang.parser.parsing.util.ParserUtils;
 
 /**
  * Generated JavaDoc Comment.
@@ -8,8 +14,37 @@ import com.intellij.lang.PsiBuilder;
  * @author Matt Nathan
  */
 public class DescribeQuery {
+
   public static boolean parse(PsiBuilder builder) {
-    // todo: implement this method
+    if (ParserUtils.lookAhead(builder, SparqlTokenTypes.KW_DESCRIBE)) {
+      final PsiBuilder.Marker describeQuery = builder.mark();
+      if (ParserUtils.getToken(builder, SparqlTokenTypes.KW_DESCRIBE, "Expecting 'DESCRIBE'")) {
+        boolean valid;
+        if (ParserUtils.getToken(builder, SparqlTokenTypes.OP_MULT)) {
+          // expected
+          valid = true;
+        } else if (Literals.parseVarOrIriRef(builder)) {
+          //noinspection StatementWithEmptyBody
+          while (Literals.parseVarOrIriRef(builder)) ;
+          valid = true;
+        } else {
+          builder.error("Expecting '*' or VarOrIRIref");
+          valid = false;
+        }
+
+        if (valid) {
+          //noinspection StatementWithEmptyBody
+          while (DatasetClause.parse(builder)) ;
+
+          WhereClause.parse(builder);
+
+          SolutionModifiers.parse(builder);
+        }
+      }
+
+      describeQuery.done(SparqlElementTypes.DESCRIBE_QUERY);
+      return true;
+    }
     return false;
   }
 }
