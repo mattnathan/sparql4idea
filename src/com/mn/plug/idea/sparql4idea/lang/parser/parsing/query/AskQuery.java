9179bd695db13973482dc6d2273cb3d6801b1dfc
Matt Nathan <matt.nathan@pressassociation.com>
2011-05-19 11:10:45 +0100

    Filled out the ask query parser

--- src/com/mn/plug/idea/sparql4idea/lang/parser/parsing/query/AskQuery.java
+++ src/com/mn/plug/idea/sparql4idea/lang/parser/parsing/query/AskQuery.java
@@ -1,15 +1,31 @@
 package com.mn.plug.idea.sparql4idea.lang.parser.parsing.query;
 
 import com.intellij.lang.PsiBuilder;
+import com.mn.plug.idea.sparql4idea.lang.lexer.SparqlTokenTypes;
+import com.mn.plug.idea.sparql4idea.lang.parser.SparqlElementTypes;
+import com.mn.plug.idea.sparql4idea.lang.parser.parsing.common.WhereClause;
+import com.mn.plug.idea.sparql4idea.lang.parser.parsing.util.ParserUtils;
 
 /**
- * Generated JavaDoc Comment.
+ * Ask parser
  *
  * @author Matt Nathan
  */
 public class AskQuery {
   public static boolean parse(PsiBuilder builder) {
-    // todo: implement this method
+    if (ParserUtils.lookAhead(builder, SparqlTokenTypes.KW_ASK)) {
+      final PsiBuilder.Marker askQuery = builder.mark();
+      if (ParserUtils.getToken(builder, SparqlTokenTypes.KW_ASK, "Expecting 'ASK'")) {
+        //noinspection StatementWithEmptyBody
+        while (DatasetClause.parse(builder));
+
+        if (!WhereClause.parse(builder)) {
+          builder.error("Expecting WhereClause");
+        }
+      }
+      askQuery.done(SparqlElementTypes.ASK_QUERY);
+      return true;
+    }
     return false;
   }
 }
